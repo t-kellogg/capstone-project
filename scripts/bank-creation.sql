c0bcf69fa7ccdc1c7053e3492aa1496c055a40b0
t-kellogg <110254651+t-kellogg@users.noreply.github.com>
2022-09-21 10:22:57 -0500

    Updated bank creation script

--- scripts/bank-creation.sql
+++ scripts/bank-creation.sql
@@ -1,20 +1,14 @@
-create table users
-(
-u_id			serial			primary key,
-username		varchar(64)		not null,
-password		varchar(64)		not null,
-email			varchar(64)		not null,
-f_name			varchar(32)		not null,
-l_name			varchar(32)		not null,
-p_no			varchar(12),
-role			int				not null
-);
-
 create table user_accounts
 (
 ua_id			serial			primary key,
 active			boolean			default true,
-user_id			int				references users(u_id) on delete restrict
+username		varchar(64)		not null,
+token			varchar(64)		not null,
+email			varchar(64)		not null,
+f_name			varchar(32)		not null,
+l_name			varchar(32)		not null,
+p_no			varchar(12),
+role			int				not NULL
 );
 
 create table bank_accounts
@@ -26,9 +20,9 @@ active			boolean			default true,
 user_account_id	int				references user_accounts(ua_id) on delete restrict
 );
 
-create table user_bank_accounts
+create table user_accounts_bank_accounts
 (
-user_id			int				references users(u_id) on delete restrict,
+user_account_id	int				references user_accounts(ua_id) on delete restrict,
 bank_account_id	int				references bank_accounts(ba_id) on delete restrict
 );
 
@@ -37,7 +31,7 @@ l_id			serial			primary key,
 action			int				not null,
 amount			numeric(13,2)	not null,
 timestamp		bigint			not null,
-user_id			int				references users(u_id) on delete restrict,
+user_account_id	int				references user_accounts(ua_id) on delete restrict,
 from_bank_id	int				references bank_accounts(ba_id) on delete restrict,
 to_bank_id		int				references bank_accounts(ba_id) on delete restrict
 );
